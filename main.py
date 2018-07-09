ba098b4f6253470625683db22f80a8274277beed
Alecks Gates <agates@mail.agates.io>
2018-07-08 21:58:51 -0500

    Add license and encoding header to relevant files.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#    domaintypesystem
+#    Copyright (C) 2018  Alecks Gates
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import asyncio
 import logging
@@ -8,13 +25,14 @@ from domaintypesystem import DomainTypeSystem
 logging.basicConfig(level=logging.DEBUG)
 
 loop = asyncio.get_event_loop()
-#loop.set_debug(True)
+# loop.set_debug(True)
 
 dts = DomainTypeSystem()
 
 
 async def print_handler(data_type_group_message, address, received_timestamp):
     logging.debug("Print handler: {0}, timestamp: {1}".format(data_type_group_message.struct_name, received_timestamp))
+
 
 asyncio.ensure_future(dts.handle_any((print_handler,)))
 
