79420048692739ded0cfa48e4ddc3707110a3fc8
Alecks Gates <agates@mail.agates.io>
2018-07-08 22:45:51 -0500

    Update setup.py to include install dependencies.

--- setup.py
+++ setup.py
@@ -1,34 +1,41 @@
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
     name='domaintypesystem',
-    version='0.0.1',
-    license='GPL-3.0',
+    version='0.0.1-r1',
+    license='GPL-3.0+',
     description='Decentralized type system',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     author='Alecks Gates',
     author_email='agates@mail.agates.io',
     keywords=[],
     url='https://gitlab.com/agates/domain-type-system',
+    python_requires='>=3.5',
     classifiers=(
-        "Development Status :: 1 - Planning",
-        "Framework :: AsyncIO",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Adaptive Technologies",
-        "Topic :: Home Automation",
-        "Topic :: Internet",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-    )
+        'Development Status :: 1 - Planning',
+        'Framework :: AsyncIO',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Adaptive Technologies',
+        'Topic :: Home Automation',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ),
+    install_requires=[
+        'blosc',
+        'capnpy==0.5.0'
+    ]
 )
