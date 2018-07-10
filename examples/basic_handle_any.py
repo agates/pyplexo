e367bd3137bb0053c0153a37f43bda75f6ae2c35
Alecks Gates <agates@mail.agates.io>
2018-07-09 19:55:17 -0500

    Rename the current only example

--- examples/basic_handle_any.py
+++ examples/basic_handle_any.py
@@ -25,13 +25,13 @@ from domaintypesystem import DomainTypeSystem
 logging.basicConfig(level=logging.DEBUG)
 
 loop = asyncio.get_event_loop()
-# loop.set_debug(True)
 
 dts = DomainTypeSystem()
 
 
 async def print_handler(data_type_group_message, address, received_timestamp):
-    logging.debug("Print handler: {0}, timestamp: {1}".format(data_type_group_message.struct_name, received_timestamp))
+    logging.debug("Print handler: {0}, address: {1}, timestamp: {2}".format(
+        data_type_group_message.struct_name, address, received_timestamp))
 
 
 asyncio.ensure_future(dts.handle_any((print_handler,)))
