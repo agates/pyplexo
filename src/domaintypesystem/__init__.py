9f502350f140ac40ae81a0cb42efbcf67cfbad38
Alecks Gates <agates@mail.agates.io>
2020-02-29 10:58:51 -0600

    Remove dts from import

--- src/domaintypesystem/__init__.py
+++ src/domaintypesystem/__init__.py
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .dts import DomainTypeSystem
+#from .dts import DomainTypeSystem
 from .receptor import *
 from .synapse import *
 from .transmitter import *
