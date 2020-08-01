90493769f9cc942839b1d177527ecce90a330f8f
Alecks Gates <agates@mail.agates.io>
2020-08-01 15:29:42 -0500

    Initial ProposalManager

--- src/plexo/ip_lease.py
+++ src/plexo/ip_lease.py
@@ -14,7 +14,6 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import ipaddress
-import random
 from typing import Union
 
 from pyrsistent import pmap
