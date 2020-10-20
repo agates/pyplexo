1328f48996553792bf7d8acf7a15a15005f9ed8f
Alecks Gates <agates@mail.agates.io>
2020-10-19 21:14:50 -0500

    Update license to Affero General Public License version 3.

--- src/plexo/ganglion/base.py
+++ src/plexo/ganglion/base.py
@@ -1,18 +1,18 @@
 #  pyplexo
-#   Copyright (C) 2020  Alecks Gates
+#   Copyright © 2018-2020  Alecks Gates
 #
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
+#  pyplexo is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
+#  pyplexo is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#  GNU Affero General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU Affero General Public License
+#  along with pyplexo.  If not, see <https://www.gnu.org/licenses/>.
 import asyncio
 from abc import ABC, abstractmethod
 from typing import Type, Optional
