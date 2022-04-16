ef6d7fb5651d94712e2f581ddd761bbfb4d2181d
Alecks Gates <agates@mail.agates.io>
2022-04-16 17:05:47 -0500

    Change license to LGPL3

--- src/plexo/transmitter.py
+++ src/plexo/transmitter.py
@@ -1,18 +1,19 @@
 #  pyplexo
-#   Copyright © 2018-2020  Alecks Gates
+#  Copyright © 2018-2022  Alecks Gates
 #
 #  pyplexo is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
+#  it under the terms of the GNU Lesser General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  pyplexo is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
+#  GNU Lesser General Public License for more details.
 #
-#  You should have received a copy of the GNU Affero General Public License
+#  You should have received a copy of the GNU Lesser General Public License
 #  along with pyplexo.  If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import annotations
 
 import asyncio
--- src/plexo/typing/transmitter.py
+++ src/plexo/typing/transmitter.py
@@ -1,3 +1,19 @@
+#  pyplexo
+#  Copyright © 2018-2022  Alecks Gates
+#
+#  pyplexo is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  pyplexo is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with pyplexo.  If not, see <https://www.gnu.org/licenses/>.
+
 from typing import Optional, Coroutine, Protocol
 from uuid import UUID
 
