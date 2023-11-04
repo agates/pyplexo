04b4acd4bf77ad2ba0843bfbe6e2e1fb5edace93
Alecks Gates <agates@mail.agates.io>
2023-11-04 03:16:43 -0500

    Add StringCodec for basic UTF- encoding/decoding

---
+++ src/plexo/codec/string_codec.py
@@ -0,0 +1,34 @@
+#  pyplexo
+#  Copyright © 2018-2022  Alecks Gates
+#
+#  pyplexo is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  pyplexo is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with pyplexo.  If not, see <https://www.gnu.org/licenses/>.
+
+from typing import cast
+
+from plexo.typing import EncodedSignal
+from plexo.typing.codec import Codec
+
+
+class StringCodec(Codec):
+    _name = "string_UTF-8"
+
+    def encode(self, data: str) -> EncodedSignal:
+        return data.encode("UTF-8")
+
+    def decode(self, data: EncodedSignal):
+        return cast(bytes, data).decode("UTF-8")
+
+    @property
+    def name(self) -> str:
+        return self._name
